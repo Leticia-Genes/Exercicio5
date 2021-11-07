a69ae6c28eb55564d6cc59098c6e82daf961a98f
Letícia Genes <leticia_genes@hotmail.com>
2021-11-06 20:32:43 -0400

    corrigido

--- ex5.ts
+++ ex5.ts
@@ -1,12 +1,18 @@
 class Palavra {
-    //conta o número de vogais da palavra recebida
+    /**
+     * Retorna a contagem do número de vogais da palavra recebida
+     * @param palavra - A string que será usada na contagem
+     */
     static contarVogais(palavra: string) : number {
-        let vogais = palavra.match(/[aeiou]/gi);
+        const vogais = palavra.match(/[aeiouàáéíóúâêôãõ]/gi);
 
-        return vogais == null ? 0 : vogais.length;
+        return vogais === null ? 0 : vogais.length;
     }
 
     //pega a palavra recebida via input e mostra o resultado do número de vogais no HTML
+    /**
+     * Mostra o número de vogais da palavra recebida via input em novo parágrafo da página HTML
+     */
     static mostrarResultado() : void {
         let palavra: string = (<HTMLSelectElement>document.getElementById('palavra')).value;
 
--- js/ex5.js
+++ js/ex5.js
@@ -1,12 +1,18 @@
 var Palavra = /** @class */ (function () {
     function Palavra() {
     }
-    //conta o número de vogais da palavra recebida
+    /**
+     * Retorna a contagem do número de vogais da palavra recebida
+     * @param palavra - A string que será usada na contagem
+     */
     Palavra.contarVogais = function (palavra) {
-        var vogais = palavra.match(/[aeiou]/gi);
-        return vogais == null ? 0 : vogais.length;
+        var vogais = palavra.match(/[aeiouàáéíóúâêôãõ]/gi);
+        return vogais === null ? 0 : vogais.length;
     };
     //pega a palavra recebida via input e mostra o resultado do número de vogais no HTML
+    /**
+     * Mostra o número de vogais da palavra recebida via input em novo parágrafo da página HTML
+     */
     Palavra.mostrarResultado = function () {
         var palavra = document.getElementById('palavra').value;
         document.body.innerHTML = "<p>Quantidade de vogais da palavra <strong>" + palavra + "</strong>: " + Palavra.contarVogais(palavra) + "</p>";
